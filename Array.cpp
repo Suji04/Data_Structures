e87fd1fa1be515aa686573f49762c5e0b4cfb61c
Sujan Dutta <33905582+Suji04@users.noreply.github.com>
2018-10-30 16:40:54 +0530

    adding array data structure

---
+++ Array.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++) cin>>arr[i];
+    for(auto i: arr) cout<<i<<" ";
+    return 0;
+}
